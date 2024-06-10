7b883bea603b031b49a0000b5679fa7ae6f0c8ac
Pavel <bosovpn47@yandex.ru>
2024-06-11 02:20:27 +0300

    fix: .dockerignore backend && frontend (.env && Dockerfile); frontend Dockerfile: added CMD ["npm", "run", "dev"]; main.py: changed port for frontend; vite.config.js: added property server for defineConfig;

--- backend/src/main.py
+++ backend/src/main.py
@@ -7,8 +7,8 @@ app = FastAPI()
 app.include_router(router_crypto)
 
 origins = [
-    "http://127.0.0.1:5174",
-    "http://localhost:5174",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
